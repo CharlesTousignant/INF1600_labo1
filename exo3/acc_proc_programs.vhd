bcf7168ef45f620dbd1ca4c3352ad8303289a664
CharlesTousignant <gigl@localhost.localdomain>
2021-02-14 18:31:26 -0500

    "Essai pour program_1 exo3"

--- exo3/acc_proc_programs.vhd
+++ exo3/acc_proc_programs.vhd
@@ -13,7 +13,7 @@ package acc_proc_programs is
       to_uint16((add,  9)),   --  4
       to_uint16((brnz, 1)),   --  5
       to_uint16((stop, 0)),   --  6
-      +8,                     --  7
+      +2,                     --  7
       +1,                     --  8
       int16_to_uint16(-1),    --  9
       others => 0
@@ -22,19 +22,25 @@ package acc_proc_programs is
    -- Programme utilise pour Q2
    constant program_1 : memtype := (
       -- ajouter votre programme ici, une ligne par instruction
-      to_uint16((ld,  == 2)) -- 0
-
-      to_uint16((adda,    == 1)) --2
-      to_uint16((adda,   ACC-2)) --3
-      to_uint16((adda,   ACC-1)) --3
+      to_uint16((lda, 13)), -- 0
       
-      to_uint16((adda,    == 1)) --2
-      to_uint16((adda,   ACC-1)) --3
+      to_uint16((ld, 12)), --1
+      to_uint16((addx, 00)), --2
+      to_uint16((suba, 12)), --3
+      to_uint16((addx, 00)), --4
+      to_uint16((suba, 12)),
+      to_uint16((sti, 00)),
 
+      int16_to_uint16((sub, 11)), --5
+      to_uint16((brz, 10)), --6
+      to_uint16((add, 11)), --7
+      to_uint16((add, 12)), --8
+      to_uint16((br, 1)), --9
 
-      to_uint16((stop, 0)),    --  FIN 
-      +1,                      -- S(0)
-      +2,                      -- S(1)
+      to_uint16((stop, 0)),    --10  FIN 
+      +11,                     --11
+      +2,                      --12 S(0)
+      +1,                      --13 S(1)
       others => 0
    );
    
